cfb7c6fb79f00c72cf1bfc7045d2787f42646899
TysonR <TysonR0319@gmail.com>
2023-02-23 14:15:00 -0500

    finished

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -4,7 +4,26 @@ import java.util.*;
 
 public class E01 {
     public int getNthFib(int n) {
-        // FIXME
-        return -1;
+        if (n <= 0) {
+            return -1; // Invalid input
+        } else if (n == 1) {
+            return 0; // F0
+        } else if (n == 2) {
+            return 1; // F1
+        } else {
+            int prevPrev = 0;
+            int prev = 1;
+            int current = 0;
+    
+            for (int i = 3; i <= n; i++) {
+                current = prev + prevPrev;
+                prevPrev = prev;
+                prev = current;
+            }
+    
+            return current;
+        }
     }
 }
+
+
